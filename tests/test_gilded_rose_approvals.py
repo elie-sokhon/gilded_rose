f5c5c3465213e811f4ac6aeeae9df69bc88289d0
elie-sokhon <elieelsokhon@gmail.com>
2025-07-09 11:07:58 +0200

    Fix flake8 excluding venv

--- tests/test_gilded_rose_approvals.py
+++ tests/test_gilded_rose_approvals.py
@@ -1,14 +1,12 @@
 import io
 import os
 import sys
+from texttest_fixture import main
 from approvaltests import verify
 from approvaltests.reporters import PythonNativeReporter
 
 # insérer le dossier parent dans sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-
-from texttest_fixture import main
 
 reporter = PythonNativeReporter()
 
--- texttest_fixture.py
+++ texttest_fixture.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+
 from gilded_rose import GildedRose
 from models import Item
 
